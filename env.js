03a3c851c1248e100e5e5e197b63bf3db0b3ff2a
wanshaobo <565020807@qq.com>
2018-11-12 23:33:12 +0800

    commit

--- env.js
+++ env.js
@@ -21,4 +21,33 @@ var table = {
 	}
 }
 
-console.log(typeof 's');;
+//线性复杂度O(n)
+var arr = [1,0,'true',true,5,false,'false','undefined',undefined,null,'null',NaN,'a','a',NaN,'NaN'];
+var newArr = arr.filter((item,index,self) => self.indexOf(item) == index)
+console.log(Number.isNaN(NaN));
+console.log(Number.isNaN(1));
+function uniqueUseNotAllEqual(array) {
+	var temp = [], mark = true;
+	for (var i = 0, j = array.length; i < j; i++) {
+		if (array[i] !== array[i]) {
+			mark && temp.indexOf(array[i]) == -1 ? temp.push(array[i]) : '';
+			mark = false;
+		} else
+			temp.indexOf(array[i]) == -1 ? temp.push(array[i]) : '';
+	}
+	return temp;
+}
+// console.log(uniqueUseNotAllEqual(arr))
+console.log(Array.from(new Set(arr)))
+/*
+ NaN有两中通用判定方法和数组中一种判定方法：
+
+ 一个是绝对不全等于(===)自身
+ 一个是ES6的isNaN()
+ 数组原型链上的Array.prototype.includes()
+ https://blog.csdn.net/xue4345/article/details/73238253
+ https://blog.csdn.net/AinUser/article/details/79953871
+ https://www.cnblogs.com/1906859953Lucas/p/9067475.html
+百度
+ [1,0,undefined,'undefined',null,NaN,NaN] js数组去重
+*/
