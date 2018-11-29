09fd154bb35661ce507883ac0969698c0403a45f
wanshaobo <wanshaobo@qiyi.com>
2018-11-29 16:33:41 +0800

    commit

--- js/algorithm.js
+++ js/algorithm.js
@@ -1,12 +1,35 @@
-//lintcode codevs hackerrank codeabbey projecteuler
+/*
+lintcode codevs hackerrank codeabbey projecteuler
 
-//快速排序算法
-//堆排序算法
-//归并排序算法
-//二分查找算法
-//线性查找算法 bfprt
-//深度优先搜索算法 dfs
-//广度优先搜索算法 bfs
-//dijkstra算法
-//动态规划算法
-//朴素贝叶斯分类算法
+快速排序算法
+堆排序算法
+归并排序算法
+二分查找算法
+线性查找算法 bfprt
+深度优先搜索算法 dfs
+广度优先搜索算法 bfs
+dijkstra算法
+动态规划算法
+朴素贝叶斯分类算法
+
+对未来的一些探索
+
+人工智能
+
+1、机器学习
+
+2、深度学习
+
+3、图像算法
+
+4、算法研究员
+
+5、图像处理
+
+6、图像识别
+
+7、语音识别
+
+*/
+
+
