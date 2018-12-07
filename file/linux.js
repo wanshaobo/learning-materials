bf6efd43f9986a4683ce0831cfc24efa6421a3f4
wanshaobo <wanshaobo@qiyi.com>
2018-12-07 17:55:51 +0800

    commit

---
+++ file/linux.js
@@ -0,0 +1,102 @@
+/*
+Linux 200多个命令
+Ubuntu-terminal
+ctrl shift + 窗口变大
+ctrl - 窗口变小
+ls 查看当前路径的文件和文件夹
+pwd
+tree /
+ls / 根目录的内容
+cd /c/Users/wanshaobo/Desktop/
+ls -a 多了.和..，称为相对路径，同时显示隐藏文件
+touch 1.txt 创建可见文件
+touch .1.txt 创建隐藏文件
+mkdir abc 创建文件夹
+ls -l 以列表方式显示（容量单位BYTE）
+ls -l -h 以列表方式显示（容量单位KB MB GB TB）
+ls -a -l -h 参数的先后顺序无所谓
+ls -alh 混合参数
+ls /c/Users/wanshaobo/Desktop/ -alh 选项+参数
+/home/wanshaobo linux系统主用户目录，也就是终端打开的默认路径
+cd change directory
+ls --help 查看命令的帮助信息
+man ls 查看命令的帮助信息 q退出 f下一屏 b上一屏
+manual 手册
+man 1 open
+man 2 open
+...
+man 8 open
+自动补全 tab
+cat 1.txt 查看文件内容
+gedit 1.txt 编辑文件
+history 查看历史命令
+!history数字 执行那个编号的命令
+rm 1.txt 删除这个文件
+ls 1* 查看1开头的文件和文件夹 通配符 *匹配0个或多个
+ls *txt
+ls 2? 以2开头的跟一个字符 ?匹配一个
+ls 2?? 以2开头的跟两个字符
+ls 1[12345]3.txt
+ls > print.txt 重定向：把原本显示在终端的内容存储到一个文件中，原文件原来的文件全部被删除 如果没有print.txt会创建
+ls >> print.txt 追加：把原本显示在终端的内容存储到一个文件的末尾 如果没有print.txt会创建
+more print.txt 以分屏方式查看文件内容 q退出 f下一屏 b上一屏
+la -alh /bin | more
+ls;ls -alh 多个命令一起执行
+cd / 两次tab键，查看这个目录下所有内容
+cd ~ 切换到当前用户的主目录/home/wanshaobo
+cd - 进入上次所在目录
+ctrl+c 取消命令 不执行命令
+tree 以目录树的方式显示文件夹中的内容
+mkdir A/B/C/test.txt -p 自动创建test.txt文件依赖的文件目录
+rmdir B 删除文件夹
+rmdir B 无法删除Directory not empty
+rm B -r 递归删除 B文件夹下面有文件 B是文件夹
+ln 源文件 链接文件 建立硬链接文件
+ln -s 源文件 链接文件 建立软链接文件
+mv 1.txt 2.txt 重命名1为2
+mv 1.txt wan/ 将1.txt文件剪切并粘贴到wan文件夹中
+mv wan/ wanshaobo 将wan文件夹重命名为wanshaobo
+cat 1.txt 2.txt > print.txt 将1和2文件的内容合并到print.txt中
+grep 文本搜索工具
+ls /bin/ > print.txt
+grep '^ntfs' print.txt 显示查找到的内容 以ntfs开头
+grep 'ntfs$' print.txt 显示查找到的内容 以ntfs结尾
+grep 'ntfs' print.txt 显示查找到的内容
+grep -n 'ntfs' print.txt 带上行号
+grep -v 'ntfs' print.txt 不包含ntfs
+cp 1.txt A 将1.txt文件复制到A文件夹下
+cp A B -r 如果是文件夹复制，需要递归操作
+find / -name '*s' 在根目录下按照文件名找文件
+*/
+
+var a = 1,b = 2;
+if(!a || !b){
+	console.log(1);
+}else{
+	console.log(2);
+}
+//2
+
+var a = 0,b = 2;
+if(!a || !b){
+	console.log(1);
+}else{
+	console.log(2);
+}
+//2
+
+var a = 1,b = 0;
+if(!a || !b){
+	console.log(1);
+}else{
+	console.log(2);
+}
+//2
+
+var a = 0,b = 0;
+if(!a || !b){
+	console.log(1);
+}else{
+	console.log(2);
+}
+//2
--- js/algorithm.js
+++ js/algorithm.js
@@ -30,6 +30,72 @@ dijkstra算法
 
 7、语音识别
 
+机器学习是什么？machine learning 算法训练模型
+已有的数据
+某种模型
+利用此模型预测未来
+机器学习届数据为王思想
+
+算法调优调参，模型预测未来越准确
+机器学习：模式识别、数据挖掘、统计学习、自然语言处理、语音识别、计算机视觉
+模式识别=机器学习
+数据挖掘=机器学习+数据库
+统计学习近似等于机器学习
+计算机视觉=图像处理+机器学习
+语音识别=语音处理+机器学习
+自然语言处理=文本处理+机器学习
+
+人工智能是机器学习的父类 ---> 机器学习是深度学习的父类 ---> 深度学习
+人工智能 -> 机器学习 -> 深度学习
+AI ML DL
+AI artificial intelligence
+ML Machine Learning
+DL deep learning
+NLP Natural Language Processing
+SR Speech Recognition
+ASR Automatic Speech Recognition
+
+深度学习框架：https://baijiahao.baidu.com/s?id=1599943447101946075&wfr=spider&for=pc
+
+google now
+iphone siri
+macrosoft cortana
+
+有监督机器学习流程：历史数据 --> 算法(公式)选择(分类算法||回归算法) --> 模型训练 --> 得到最优解 --> 模型 --> 新数据 --> 预测结果
+数据分为训练数据集和测试数据集
+data：train set &&  test set
+
+有监督机器学习训练流程：
+
+机器学习库Scikit-Learn
+NumPy系统是Python的一种开源的数值计算扩展。这种工具可用来存储和处理大型矩阵，比Python自身的嵌套列表（nested list structure)结构要高效的多（该结构也可以用来表示矩阵（matrix））。
+pandas 是基于NumPy 的一种工具，该工具是为了解决数据分析任务而创建的。Pandas 纳入了大量库和一些标准的数据模型，提供了高效地操作大型数据集所需的工具。pandas提供了大量能使我们快速便捷地处理数据的函数和方法。你很快就会发现，它是使Python成为强大而高效的数据分析环境的重要因素之一。
+Scikit-Learn基于NumPy封装的一些算法，让工程师使用算法更加简单，不在需要NumPy构建算法实现公式
+
+机器学习工程师需要做的事情：
+有监督
+Classification 分类
+Regression 回归
+无监督
+Clustering 聚类
+Dimensionality reduction 降维
+Model selection 模型选择
+Preprocessing 预处理
+
+回归
+线性回归
+线性：两个变量之间存在一次方函数关系
+
+机器学习：让机器像人具备学习的能力
+算法是公式，把数据代到算法里面是模型，模型就是参数
+预测：把新的数据和参数进行计算得到结果
+
+*/
+
+/*
+重点研究无人驾驶和导航
+Python全栈开发 人工智能
+https://ke.qq.com/course/348490?tuin=369cc19
 */
 
 
