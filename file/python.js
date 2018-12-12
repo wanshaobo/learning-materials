d7959aa5b9bfd2f0cf49805a3c9dd3781da352b3
wanshaobo <wanshaobo@qiyi.com>
2018-12-12 18:20:29 +0800

    commit

---
+++ file/python.js
@@ -0,0 +1,46 @@
+/*
+爬虫 服务器 AI
+不需要编译直接执行，因为有Python解释器
+gcc test.c
+./a.out
+
+sudo apt install python3
+sudo apt install python
+sudo apt install python-minimal
+
+python 进入python交互模式
+exit() 退出
+
+python3
+
+ipython 支持命令ls ll等
+exit
+
+ipython3 支持命令ls ll等
+exit
+
+#表示注释
+'''
+多行注释
+'''
+"""
+多行注释
+"""
+
+python无法执行带有中文字符的程序
+#coding=utf-8
+#-*- coding:utf-8 -*-
+
+height = input("请输入你的身高");
+
+height = 18
+print("height:%d"%height)
+
+name = 'wan'
+print("name:%s"%name)
+
+python2中input采集并执行，python3中input采集并存储
+
+python2
+a = raw_input("请输入：")
+*/
